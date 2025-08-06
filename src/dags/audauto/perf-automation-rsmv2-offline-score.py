cb3abe7d65a11c8bd40b17e3c1ec7a143fb77d0c
YananGao <36028431+Yanan-Gao@users.noreply.github.com>
2025-08-06 10:14:20 +0800

    Route skipped EMR branches through post-processing (#46)

--- src/dags/audauto/perf-automation-rsmv2-offline-score.py
+++ src/dags/audauto/perf-automation-rsmv2-offline-score.py
@@ -214,7 +214,7 @@ emr_cluster_part1 = utils.create_emr_cluster(
 )
 
 # Confetti prepare/gate for the Imp2Br model input generation
-prep_imp2br, gate_imp2br = make_confetti_tasks(
+prep_imp2br, gate_imp2br, run_imp2br, skip_imp2br = make_confetti_tasks(
     group_name="audience",
     job_name="Imp2BrModelInferenceDataGenerator",
     experiment_name=experiment,
@@ -259,7 +259,7 @@ emr_cluster_part2 = utils.create_emr_cluster(
 )
 
 # Prepare Confetti runtime config for all part2 jobs
-prep_part2, gate_part2 = make_confetti_tasks(
+prep_part2, gate_part2, run_part2, skip_part2 = make_confetti_tasks(
     group_name="audience",
     job_name="RelevanceModelOfflineScoringPart2",
     experiment_name=experiment,
@@ -377,9 +377,14 @@ data_quality_check = utils.create_emr_spark_job(
     emr_cluster_part2,
 )
 
-rsm_etl_dag >> dataset_sensor >> prep_imp2br >> gate_imp2br >> emr_cluster_part1 >> model_sensor >> copy_feature_json >> clean_up_raw_embedding >> prep_part2 >> gate_part2 >> emr_cluster_part2
+rsm_etl_dag >> dataset_sensor >> prep_imp2br >> gate_imp2br
+gate_imp2br >> run_imp2br >> emr_cluster_part1 >> model_sensor >> copy_feature_json >> clean_up_raw_embedding >> prep_part2 >> gate_part2
+gate_imp2br >> skip_imp2br >> post_processing_imp2br
+emr_cluster_part1 >> post_processing_imp2br
+gate_part2 >> run_part2 >> emr_cluster_part2
+gate_part2 >> skip_part2 >> post_processing_part2
+emr_cluster_part2 >> post_processing_part2
 emb_gen >> emb_aggregation >> emb_to_coldstorage >> emb_dot_product >> score_min_max_scale_population >> data_quality_check
 final_dag_check = OpTask(op=FinalDagStatusCheckOperator(dag=adag))
-emr_cluster_part2 >> final_dag_check
-emr_cluster_part1 >> post_processing_imp2br >> final_dag_check
-emr_cluster_part2 >> post_processing_part2 >> final_dag_check
+post_processing_imp2br >> final_dag_check
+post_processing_part2 >> final_dag_check
